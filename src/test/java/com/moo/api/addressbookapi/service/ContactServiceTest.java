2cf5ba787bad88880bb6be6f90aa9acf0b23308e
Nodir <nodirsid@gmail.com>
2020-03-03 12:55:11 +0000

    Updated tests

--- src/test/java/com/moo/api/addressbookapi/service/ContactServiceTest.java
+++ src/test/java/com/moo/api/addressbookapi/service/ContactServiceTest.java
@@ -41,6 +41,26 @@ public class ContactServiceTest {
         contactService.addContact(newContact);
         Assert.assertTrue(newContact.equals(contactService.retrieveContact(newContact.getId())));
     }
+    
+    @Test
+    public void testPositiveSearchContact() {
+        Contact newContact = createNewContact();
+        contactService.addContact(newContact);
+        List<Long> foundContactIds = contactService.searchContacts("Anderson");
+        System.out.println(foundContactIds);
+        Assert.assertTrue(foundContactIds.containsAll(Arrays.asList(1L, 5L, 6L, 8L)));
+        Assert.assertTrue(Arrays.asList(1L, 5L, 6L, 8L).containsAll(foundContactIds));
+    }
+    
+    @Test
+    public void testNegativeSearchContact() {
+        Contact newContact = createNewContact();
+        contactService.addContact(newContact);
+        List<Long> foundContactIds = contactService.searchContacts("Oracle");
+        System.out.println(foundContactIds);
+        Assert.assertTrue(foundContactIds.containsAll(Arrays.asList()));
+        Assert.assertTrue(Arrays.asList().containsAll(foundContactIds));
+    }
 
     private Contact createNewContact() {
         return new Contact("Thomas", "Anderson", 33, "1 Road Plaza, Buffalo, NY 12654, USA");
